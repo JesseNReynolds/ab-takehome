f8d27e8ec09c3e88cc977ac1d29c02f1a02b785d
Jesse Reynolds <jessenreynolds@gmail.com>
2023-03-08 16:46:15 -0700

    landing page message

--- src/Views/LandingPage/LandingPage.js
+++ src/Views/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ function LandingPage() {
   return (
     <Box>
       <Typography variant="h3" color="primary" align='center'>Thanks!</Typography>
-      <Typography variant="body1" color="initial" align='center'> I appreciate the chance to show what I can do in this takehome. I hope you're pleased. I used ReactRouter and MaterialUI.</Typography>
+      <Typography variant="body1" color="initial" align='center'> I appreciate the chance to show what I can do in this takehome. I hope you're pleased. I used ReactRouter and MaterialUI, I've never used materialUI before, but this struck me as a good time to give it a shot.</Typography>
     </Box>
   )
 }
