15843b5eb51f0bfe27d6d8c3f0c993e7ba6f8f1c
Jesse Reynolds <jessenreynolds@gmail.com>
2023-03-08 15:13:36 -0700

    landing page update

--- src/Views/LandingPage/LandingPage.js
+++ src/Views/LandingPage/LandingPage.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
+import { Box } from '@mui/system'
+
 
 function LandingPage() {
   return (
-    <>
-    <Typography variant="h1" color="initial">Landing Page</Typography>
-    </>
+    <Box>
+      <Typography variant="h3" color="primary" align='center'>Thanks!</Typography>
+      <Typography variant="body1" color="initial" align='center'> I appreciate the chance to show what I can do in this takehome. I hope you're pleased. I used ReactRouter and MaterialUI.</Typography>
+    </Box>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  createBrowserRouter,
-  createProvider,
-  RouterProvider
+  BrowserRouter
 } from "react-router-dom";
-import LandingPage from './Views/LandingPage/LandingPage';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage/>,
-  },
-]);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
   </React.StrictMode>
 );
