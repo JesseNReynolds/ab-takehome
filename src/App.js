6c53fbf0989bee5d93be08706df124aafe9327ff
Jesse Reynolds <jessenreynolds@gmail.com>
2023-03-08 17:28:25 -0700

    grabbed a navbar from material demo, gotta overwrite click behavior

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { Route, Routes } from 'react-router-dom'
 import LandingPage from './Views/LandingPage/LandingPage'
 import MessagesIndex from './Views/MessagesIndex/MessagesIndex'
 import UsersIndex from './Views/UsersIndex/UsersIndex'
+import NavBar from './Views/NavBar/NavBar.js'
+import { Stack } from '@mui/system'
 
 function App() {
   return (
-    <Routes>
-        <Route path="/" element={<LandingPage/>} />
-        <Route path='/users' element={<UsersIndex/>} />
-        <Route path='/messages' element={<MessagesIndex/>} />
-    </Routes>
+    <Stack>
+      <NavBar/>
+      <Routes>
+          <Route path="/" element={<LandingPage/>} />
+          <Route path='/users' element={<UsersIndex/>} />
+          <Route path='/messages' element={<MessagesIndex/>} />
+      </Routes>
+    </Stack>
   )
 }
 
