adb696b82253f894c44fa6b8544ab7932e279dbc
Jesse Reynolds <jessenreynolds@gmail.com>
2023-03-09 04:56:31 -0700

    more hashrouter stuff

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import {
-  HashRouter
-} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from './App';
 
