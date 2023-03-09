39a2b14b4fdd821429bffb6c62801c1b5fe06593
Jesse Reynolds <jessenreynolds@gmail.com>
2023-03-09 04:46:24 -0700

    trying hashrouter to get around github pages weirdness

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import {
-  BrowserRouter
+  HashRouter
 } from "react-router-dom";
 
 import App from './App';
@@ -11,8 +11,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App></App>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
